ff9530518cb159c0ea01d96f6198d9cd4cdad964
4damW <81177099+4damW@users.noreply.github.com>
2021-03-22 16:00:32 +0000

    Add files via upload

---
+++ CreateRenewLetter/CreateRenewLetter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CreateRenewLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CsvData dataList = new CsvData();
+
+            int records = dataList.recordCount();
+
+            GetData[] data = new GetData[records];
+            DataCalculation[] calculate = new DataCalculation[records];
+            CreateTextFile[] text = new CreateTextFile[records];
+
+            for (int index = 0; index < records; index++)
+            {
+                data[index] = new GetData(dataList.getCsvRecord(index));
+                calculate[index] = new DataCalculation(data[index].getAnnualPremium());
+                text[index] = new CreateTextFile(data[index], calculate[index]);
+            }
+
+            Console.Write("\nProgram has finnished. Press ENTER to exit...");
+            Console.ReadLine();
+        }
+    }
+}
